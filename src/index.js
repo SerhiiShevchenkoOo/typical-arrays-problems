c63fe7b8f095ff224230bdadd2121159499786d7
Serhii Shevchenko <serhiii.shevchenko@gmail.com>
2020-09-28 16:14:42 +0300

    init: start array-problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (typeof (array) == 'undefined') return 0
+    else if (array.length == 0 || Array.isArray(array == false)) return 0
+    else return array.sort((a, b) => a - b)[0]
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (typeof (array) == 'undefined') return 0
+    else if (array.length == 0 || Array.isArray(array == false)) return 0
+    else return array.sort((a, b) => b - a)[0]
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (typeof (array) == 'undefined') return 0
+    else if (array.length == 0 || Array.isArray(array == false)) return 0
+    else return array.reduce((sum, current) => (sum + current)) / array.length
 }
